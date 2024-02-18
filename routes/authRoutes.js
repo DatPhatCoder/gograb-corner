9bd970ef058f735c25ed513fbf06e222e0c41f28
hoohamm_ <49733343+DatPhatCoder@users.noreply.github.com>
2024-02-18 14:48:18 -0800

    Clarified that 3rd party api should be used for google login in authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,7 @@
 //demo - authRoutes - brief idea of what needs to be done with each module
 
+//use 3rd party api for google login
+
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
