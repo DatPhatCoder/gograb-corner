80badc2ae36396e56fac2c03754fae08b5fd4a94
hoohamm_ <49733343+DatPhatCoder@users.noreply.github.com>
2024-02-18 02:23:42 -0800

    Add restarted server to make it more organized

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    console.log('have');
+    res.status(500).json({ message: 'Error' });
+    res.send('hi');
+})
+
+app.listen(3000);
